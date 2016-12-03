d9f62aaa475f54df941f1647da9e662512cc640b
Kevin Wirth <me@wirthdesign.com>
2016-12-03 16:54:39 -0500

    Lecture 85

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<router-outlet></router-outlet>'
+  template: `
+    <navbar></navbar>
+    <router-outlet></router-outlet>
+  `
 })
 
 export class AppComponent { }
